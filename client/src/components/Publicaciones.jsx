fda6107a2a3a4212a987ab1f91a27d4d4568a004
Andresx2005 <andresvoet10@gmail.com>
2023-11-29 15:49:23 -0300

    form publicaciones

--- client/src/components/Publicaciones.jsx
+++ client/src/components/Publicaciones.jsx
@@ -1,8 +1,22 @@
+import "./publicaciones.css"
+
 function Publicaciones() {
 
     return(
         <>
-            <p>Publicaciones</p>
+            <div className="contenedor">
+                <div id="form-publicaciones">
+                    <h3><u>Publicación</u></h3>
+                    <form>
+                        <label>Nombre:</label><br></br>
+                        <input placeholder="Ingrese su nombre" required></input><br></br><br></br>
+                        <label>Descripción:</label><br></br>
+                        <textarea placeholder="(Mensaje, redes, discapacidad, etc... )"></textarea><br></br><br></br>
+                        <label>Imagen: </label><br></br>
+                        <input type="file" accept=".jpg, .png" required/>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
